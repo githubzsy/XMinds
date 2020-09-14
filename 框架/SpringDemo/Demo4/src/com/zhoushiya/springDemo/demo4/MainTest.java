b9b2a34ca8f97c36461e2a4559a828a0fadd174d
周诗雅 <392587086@qq.com>
2020-09-14 16:01:38 +0800

    AOP操作-Aspect注解

--- 框架/SpringDemo/Demo4/src/com/zhoushiya/springDemo/demo4/MainTest.java
+++ 框架/SpringDemo/Demo4/src/com/zhoushiya/springDemo/demo4/MainTest.java
@@ -1,9 +1,18 @@
 package com.zhoushiya.springDemo.demo4;
 
+import com.zhoushiya.springDemo.demo4.aopAnnotation.User;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author zhoushiya
  * @date 2020/9/14 11:53
  */
 public class MainTest {
-
+    @Test
+    public void testAop(){
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("bean1.xml");
+        User user = classPathXmlApplicationContext.getBean("user", User.class);
+        user.add();
+    }
 }
