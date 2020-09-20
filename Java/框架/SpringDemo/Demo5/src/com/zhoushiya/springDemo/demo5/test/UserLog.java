b7f5bc00c1a5120215d7159b9f55261f6eab4673
周诗雅 <392587086@qq.com>
2020-09-20 16:46:46 +0800

    Spring5 - 整合log4j2

---
+++ Java/框架/SpringDemo/Demo5/src/com/zhoushiya/springDemo/demo5/test/UserLog.java
@@ -0,0 +1,17 @@
+package com.zhoushiya.springDemo.demo5.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhoushiya
+ * @date 2020/9/20 16:42
+ */
+public class UserLog {
+
+    private static final Logger log = LoggerFactory.getLogger(UserLog.class);
+
+    public static void main(String[] args) {
+        log.info("main函数");
+    }
+}
