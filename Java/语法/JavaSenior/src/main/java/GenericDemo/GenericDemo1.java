a01deb1c586793106a7e826c6e270e2c98286972
周诗雅 <392587086@qq.com>
2020-09-28 11:06:52 +0800

    JVM - 添加泛型相关说明

---
+++ Java/语法/JavaSenior/src/main/java/GenericDemo/GenericDemo1.java
@@ -0,0 +1,13 @@
+package GenericDemo;
+
+import java.util.List;
+
+/**
+ * @author zhoushiya
+ * @date 2020/9/28 10:28
+ */
+public class GenericDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
