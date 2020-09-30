8e56a9d4853e8cfd178421cf2ae5f8139b835e47
周诗雅 <392587086@qq.com>
2020-09-30 22:02:55 +0800

    SpringCloud - 完成SpringCloudBus示例

---
+++ Java/框架/SpringCloudDemo/cloud-eureka-config-client3356/src/main/java/com/zhoushiya/springcloud/EurekaConfigClientMain3356.java
@@ -0,0 +1,17 @@
+package com.zhoushiya.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zhoushiya
+ * @date 2020/9/30 20:06
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaConfigClientMain3356 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigClientMain3356.class,args);
+    }
+}
