18047f19a0259adddac53df3af418032b5fc6069
周诗雅 <392587086@qq.com>
2020-09-30 15:14:40 +0800

    C# - .Net 5 添加CustomActionFilter

---
+++ C#/.Net 5/DotNet5Demo/Web/Filters/CustomActionFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Filters
+{
+    public class CustomActionFilterAttribute :Attribute, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            context.HttpContext.Response.WriteAsync("******OnActionExecuted******");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            string user = context.HttpContext.Session.GetString("User");
+            if (string.IsNullOrEmpty(user))
+            {
+                context.HttpContext.Session.SetString("User", $"张三");
+            }
+
+            context.HttpContext.Response.WriteAsync("******OnActionExecuting******");
+        }
+    }
+}
