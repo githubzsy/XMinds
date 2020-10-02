26fa96e90ff4fbb6edd8e20b4c544ef9a59808c9
周诗雅 <392587086@qq.com>
2020-10-02 21:06:40 +0800

    SpringCloud - 完成Nacos作为配置中心的演示

---
+++ Java/框架/SpringCloudDemo/cloudalibaba-config-nacos-client3377/src/main/java/com/zhoushiya/springcloudalibaba/ConfigNacosClientMain3377.java
@@ -0,0 +1,17 @@
+package com.zhoushiya.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zhoushiya
+ * @date 2020/10/2 10:20
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigNacosClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigNacosClientMain3377.class,args);
+    }
+}
